9e8509b32b5b41c38fe734705358424ccb253dd1
ahhjm881 <game.rcandyr@gmail.com>
2024-11-22 22:21:39 +0900

    feat: 잡다한거 수정
    캐릭터 시작 위치 수정 안 된거 다시 적용
    플레이어 연성 애니메이션 적용(making_1,2 나눈거 적용)
    아히르 챕터 1 제출 대사 선택지 순서 바뀐거 수정
    테스트용 오프닝/엔딩 스킵(엔터 키) 구현(빌드시 제거하지는 않음)

--- Unity/Assets/Dev/Script/Event/MapEvent/EndCreditToMainMenu.cs
+++ Unity/Assets/Dev/Script/Event/MapEvent/EndCreditToMainMenu.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class EndCreditToMainMenu : MonoBehaviour
 {
+    private bool _isMoved;
     private void Awake()
     {
         ScreenManager.Instance.CurrentCursor = CursorType.None;
+
+        UniTask.WaitUntil(
+            () => Input.GetKeyDown(KeyCode.Return),
+            PlayerLoopTiming.Update,
+            this.GetCancellationTokenOnDestroy()
+        ).ContinueWith(() =>
+        {
+            if (_isMoved) return;
+            ToMainMenu();
+        }).Forget(e =>
+        {
+            if (e is not OperationCanceledException)
+            {
+                Debug.LogException(e);
+            }
+        });
     }
 
     public void ToMainMenu()
     {
         if (SceneLoader.Instance)
         {
+            _isMoved = true;
             SceneLoader.Instance
                 .WorkDirectorAsync(false, "BlackAlphaEndCredit")
                 .ContinueWith(_ => SceneLoader.Instance.LoadWorldAsync("World_MainMenu"))
--- Unity/Assets/Dev/Script/Event/MapEvent/OpeningToWorld.cs
+++ Unity/Assets/Dev/Script/Event/MapEvent/OpeningToWorld.cs
@@ -1,8 +1,6 @@
-
-
-
 using System;
 using System.Collections;
+using System.Threading;
 using Cysharp.Threading.Tasks;
 using ProjectBBF.Persistence;
 using UnityEngine;
@@ -13,10 +11,21 @@ public class OpeningToWorld : MonoBehaviour
 
     private void Awake()
     {
-        UniTask
-            .Delay(TimeSpan.FromSeconds(_waitDuration), DelayType.Realtime, PlayerLoopTiming.Update,
-                this.GetCancellationTokenOnDestroy())
-            .ContinueWith(async () =>
+        var task0 = UniTask.WaitUntil(
+            () => Input.GetKeyDown(KeyCode.Return),
+            PlayerLoopTiming.Update,
+            this.GetCancellationTokenOnDestroy()
+        );
+
+        var task1 = UniTask.Delay(
+            TimeSpan.FromSeconds(_waitDuration),
+            DelayType.Realtime,
+            PlayerLoopTiming.Update,
+            this.GetCancellationTokenOnDestroy()
+        );
+
+        UniTask.WhenAny(task0, task1)
+            .ContinueWith(async __ =>
             {
                 string scene = "World_Festival_Ch_0";
 
@@ -24,7 +33,11 @@ public class OpeningToWorld : MonoBehaviour
                 _ = await SceneLoader.Instance.LoadWorldAsync(scene);
                 _ = await SceneLoader.Instance.LoadImmutableScenesAsync();
                 _ = await SceneLoader.Instance.WorkDirectorAsync(true, "BlackAlpha");
-            }).Forget(Debug.LogError);
+            }).Forget(e=>{
+                if (e is not OperationCanceledException)
+                {
+                    Debug.LogException(e);
+                }
+            });
     }
-    
 }
